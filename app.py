6177ea0af4ee6e39eb27ddeb612e34e46c7bd6b3
Дмитрий Гриднев <flymedllva@gmail.com>
2019-11-25 10:18:52 +0300

    Поправил асинхронность, добавил поддержку постоянного соединения, статическую директорию, поддержку многих типов файлов, поиск favicon в директории сервера

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
 from server import HTTPServer, HTTPTemplate
-from config import *
+from config import Config
 
-server = HTTPServer(host=Config.host.value, port=Config.port.value, static="./static")
+server = HTTPServer(**{i.name: i.value for i in Config})
 
 
 @server.route("/", methods=["GET"])
 def index():
-    return HTTPTemplate.html("<h1>Hello h1</h1><h2>Hello h2</h2>")
+    html = "<h1>Hello h1</h1><h2>Hello h2</h2><a href='/static/1.jpg'><img src='/static/1.jpg' height='20%'></a>" \
+           "<br><br><audio controls><source src='/static/1.mp3' type='audio/mpeg'>!!!<a href='/static/1.mp3'>" \
+           "Скачайть</a></audio>"
+    return HTTPTemplate.use(html, type="HTML")
 
 
 # В браузере GET вызовет 405
 @server.route("/test.html", methods=["POST"])
 def index():
-    return HTTPTemplate.html("<h1>Hello h1</h1><h2>Hello h2</h2>")
+    return HTTPTemplate.use("<h1>Hello h1</h1><h2>Hello h2</h2>", type="HTML")
 
 
 @server.route("/test.json", methods=["GET"])
 def test_json():
     with open("./static/j.json", "r") as j:
-        return HTTPTemplate.other_type("".join(j.readlines()), type="JSON")
+        return HTTPTemplate.use("".join(j.readlines()), type="JSON")
 
 
 @server.route("/logs.log", methods=["GET"])
 def logs():
     try:
         with open("server.log", "r") as f:
-            return HTTPTemplate.other_type("".join(f.readlines()), type="None")
+            return HTTPTemplate.use("".join(f.readlines()), type="None")
     except FileNotFoundError:
-        return HTTPTemplate.html("")
+        return HTTPTemplate.use("", type="None")
+
+
+@server.route("/1.jpg", methods=["GET"])
+def jpg_1():
+    with open("./static/1.jpg", "rb") as f:
+        return HTTPTemplate.use(f.read(), type="JPG")
 
 
 server.run()
--- server.py
+++ server.py
@@ -1,26 +1,39 @@
 import asyncio
 import logging
+import os
 
 from datetime import datetime
 
 
 class HTTPTemplate(object):
 
-    @staticmethod
-    def html(template: str) -> dict:
+    @classmethod
+    def determine_type(cls, path: str, supported_formats: tuple = None) -> dict or None:
         """
-        Создает темплейт для HTML страницы
+        Определяет тип файла и возвращает его темплейт или None
 
-        :param template:
+        :param path:
+        :param supported_formats: для запрета всех форматов пустой словарь
         :return:
         """
 
-        return dict(site=template, type="HTML")
+        expansion = path.split(".")[-1].upper()
+
+        if supported_formats is None:
+            supported_formats = ("HTML", "JPG", "JPEG", "PNG", "ICO", "JSON", "SVG", "SVGZ", "WOFF", "WOFF2", "MP4",
+                                 "MP3", "TTF", "JPG", "JPEG", "GIF", "PNG", "ICO", "SH", "AVI", "BIN", "CSS", "CSV",
+                                 "EPUB" "ZIP", "RAR", "DOC", "XLS", "PPT", "MID", "MIDI", "WAV", "RTF", "ICS", "JS",
+                                 "MPKG", "PDF", "TAR", "XML")
+
+        if any(expansion == item for item in supported_formats):
+            with open(path, "rb") as f:
+                return HTTPTemplate.use(f.read(), type=expansion)
+        return None
 
     @staticmethod
-    def other_type(template: any, type: str) -> dict:
+    def use(template: any, type: str = None) -> dict:
         """
-        Создает темплйет для выбранного типа страницы
+        Создает темплейт из функции
 
         :param template:
         :param type: HTML
@@ -32,21 +45,22 @@ class HTTPTemplate(object):
 
 class HTTPHandler(asyncio.Protocol):
 
-    def __init__(self, server: 'HTTPServer'):
-        self.host = None
-        self.port = None
-        self.transport = None
-        self.method = None
-        self.allowed_methods = ["GET"]
-        self.url = None
-        self.type = None
-        self.code = None
-        self.headers = None
-        self.content = None
-        self.body = None
-        self._server = server
+    def __init__(self, server: 'HTTPServer', loop: asyncio.get_event_loop):
+        self.__host = None
+        self.__port = None
+        self.__transport = None
+        self.__method = None
+        self.__allowed_methods = ["GET"]
+        self.__url = None
+        self.__type = None
+        self.__code = None
+        self.__headers = None
+        self.__content = None
+        self.__body = None
+        self.__server = server
+        self.__loop = loop
 
-    def connection_made(self, transport) -> None:
+    def connection_made(self, transport: asyncio.selector_events) -> None:
         """
         Обрабатывает подключение
 
@@ -54,12 +68,32 @@ class HTTPHandler(asyncio.Protocol):
         :return:
         """
 
-        self.transport = transport
-        self.host = self.transport.get_extra_info('peername')[0]
-        self.port = self.transport.get_extra_info('peername')[1]
-        logging.info(f"{self.host}:{self.port} - New request")
+        self.__transport = transport
+        self.__host = self.__transport.get_extra_info('peername')[0]
+        self.__port = self.__transport.get_extra_info('peername')[1]
+        logging.info(f"{self.__host}:{self.__port} - New connection")
 
-    def data_received(self, data) -> None:
+    def eof_received(self) -> None:
+        """
+        Вызывается, когда клиент закрывает соединение
+
+        :return:
+        """
+
+        self.__transport.close()
+        logging.info(f"{self.__host}:{self.__port} - Сlient closed the connection to the server")
+
+    def connection_lost(self, exc: any) -> None:
+        """
+        Вызывается, когда сервер закрывает соединение
+
+        :return:
+        """
+
+        self.__transport.close()
+        logging.info(f"{self.__host}:{self.__port} - Server closed connection peer socket")
+
+    def data_received(self, data: bytes) -> None:
         """
         Принимает данные подключения
 
@@ -67,23 +101,26 @@ class HTTPHandler(asyncio.Protocol):
         :return:
         """
 
-        msg = data.decode()
+        data = data.decode()
+        self.__loop.create_task(self._generate_and_send_page(data))
 
-        msg = self.__generate_response(msg)
+    async def _generate_and_send_page(self, data: str):
+        """
+        Генерирует страницу и отправляет ее клиенту
 
-        logging.info(f"{self.host}:{self.port} - Data received (code: {self.code}) {self.url}")
+        :param data:
+        :return:
+        """
 
-        self.transport.write(
-            msg
-        )
+        data = await self.__generate_response(data)
 
-        logging.info(f"{self.host}:{self.port} - Data is sent")
+        logging.info(f"{self.__host}:{self.__port} - Data received ({self.__method}: {self.__code}) {self.__url}")
 
-        self.transport.close()
+        self.__transport.write(data)
 
-        logging.info(f"{self.host}:{self.port} - Close the peer socket")
+        logging.info(f"{self.__host}:{self.__port} - Data is sent ({len(data)} bytes)")
 
-    def __generate_response(self, request: str) -> bytes:
+    async def __generate_response(self, request: str) -> bytes:
         """
         Генерирует ответ
 
@@ -91,27 +128,44 @@ class HTTPHandler(asyncio.Protocol):
         :return:
         """
 
-        self.method, self.url = request.split(" ")[:2]
+        self.__method, self.__url = request.split(" ")[:2]
 
-        # if find
+        # Проверяет не favicon ли запрашивает сервер
+        if self.__url == "/favicon.ico":
+            # Поиск favicon в корне и /static
+            if os.path.isfile(self.__server.path + self.__url):
+                self.__content = HTTPTemplate.determine_type(self.__server.path + self.__url,
+                                                             supported_formats=self.__server.static_supported_format)
+            elif os.path.isfile(self.__server.path + self.__server.static + self.__url):
+                self.__content = HTTPTemplate.determine_type(self.__server.static_path + self.__url,
+                                                             supported_formats=self.__server.static_supported_format)
+            else:
+                self.__code = 404
+        # Проверяет существование файла в статическом хранилище
+        elif self.__url.startswith(self.__server.static):
+            if os.path.isfile(self.__server.path + self.__url):
+                self.__content = HTTPTemplate.determine_type(self.__server.path + self.__url,
+                                                             supported_formats=self.__server.static_supported_format)
+                if self.__content is None:
+                    self.__code = 403
+            else:
+                self.__code = 404
+        # Если предыдущие не выполнились ищет в роутах
+        else:
+            try:
+                # Получает функцию из route для текущей ссылки и получает разрешенные методы
+                if server_response := self.__server.check_route(self.__url):
+                    # Запускает функцию из route
+                    self.__content = server_response["route"]()
+                    # Разрешенные методы
+                    if "methods" in server_response:
+                        self.__allowed_methods = server_response["methods"]
+            except Exception as e:
+                logging.error(f"{self.__host}:{self.__port} - {e}")
 
-        # Получает функцию из route для текущей ссылки и разрешенные методы
-        server_response = self._server.check_route(self.url)
+        return await self.__generate_page()
 
-        # Запускает функцию из route
-        try:
-            if server_response:
-                self.content = server_response["route"]()
-        except Exception as e:
-            logging.error(f"{self.host}:{self.port} - {e}")
-
-        # Разрешенные методы
-        if server_response is not None and "methods" in server_response:
-            self.allowed_methods = server_response["methods"]
-
-        return self.__generate_page().encode()
-
-    def __generate_page(self) -> str:
+    async def __generate_page(self) -> bytes:
         """
         Генерирует страницу и возращает ее
 
@@ -122,7 +176,9 @@ class HTTPHandler(asyncio.Protocol):
         self.__generate_body()
         self.__generate_header()
 
-        return self.headers + "\r\n" + self.body
+        if isinstance(self.__body, bytes):
+            return self.__headers.encode() + self.__body
+        return (self.__headers + self.__body).encode()
 
     def __generate_code(self) -> None:
         """
@@ -131,15 +187,27 @@ class HTTPHandler(asyncio.Protocol):
         :return:
         """
 
-        if not self.content:
-            self.headers = f"HTTP/1.1 404 Not found\n"
-            self.code = 404
-        elif self.method not in self.allowed_methods:
-            self.headers = f"HTTP/1.1 405 Method not allowed\n"
-            self.code = 405
+        http_version = "HTTP/1.1"
+
+        if self.__code:
+            if self.__code == 403:
+                self.__headers = f"{http_version} 403 Forbidden\n"
+            elif self.__code == 404:
+                self.__headers = f"{http_version} 404 Not found\n"
+            elif self.__code == 405:
+                self.__headers = f"{http_version} 405 Method not allowed\n"
+            elif self.__code == 200:
+                self.__headers = f"{http_version} 200 OK\n"
         else:
-            self.headers = f"HTTP/1.1 200 OK\n"
-            self.code = 200
+            if not self.__content:
+                self.__headers = f"{http_version} 404 Not found\n"
+                self.__code = 404
+            elif self.__method not in self.__allowed_methods:
+                self.__headers = f"{http_version} 405 Method not allowed\n"
+                self.__code = 405
+            else:
+                self.__headers = f"{http_version} 200 OK\n"
+                self.__code = 200
 
     def __generate_body(self) -> None:
         """
@@ -148,15 +216,23 @@ class HTTPHandler(asyncio.Protocol):
         :return:
         """
 
-        if self.code == 404 or not self.content:
-            self.body = "<h1>404</h1><p>Not found</p>"
-            self.type = "HTML"
-        elif self.code == 405:
-            self.body = "<h1>405</h1><p>Method not allowed</p>"
-            self.type = "HTML"
+        if self.__code == 403:
+            self.__body = "<div><h1>403</h1><p>Forbidden</p></div>"
+            self.__type = "HTML"
+        elif self.__code == 404 or not self.__content:
+            self.__body = "<div><h1>404</h1><p>Not found</p></div>"
+            self.__type = "HTML"
+        elif self.__code == 405:
+            self.__body = "<div><h1>405</h1><p>Method not allowed</p></div>"
+            self.__type = "HTML"
+            self.__headers += f"Allow: {', '.join(self.__allowed_methods)}\n"
+        elif self.__code == 501:
+            self.__body = "<div><h1>501</h1><p>Not Implemented</p></div>"
+            self.__type = "HTML"
+            self.__headers += f"Allow: {', '.join(self.__allowed_methods)}\n"
         else:
-            self.body = self.content["site"]
-            self.type = self.content["type"]
+            self.__body = self.__content["site"]
+            self.__type = self.__content["type"]
 
     def __generate_header(self) -> None:
         """
@@ -165,35 +241,118 @@ class HTTPHandler(asyncio.Protocol):
         :return:
         """
 
-        self.headers += f"Server: {self._server.name}\n"
-        self.headers += f"Date: {self._server.http_date(datetime.now())}\n"
-        self.headers += f"Content-Length: {len(self.body)}\n"
+        self.__headers += f"Server: {self.__server.name}\n"
+        self.__headers += f"Date: {self.__server.http_date(datetime.now())}\n"
+        self.__headers += "Accept-Charset: utf-8\n"
+        self.__headers += f"Content-Length: {len(self.__body)}\n"
+        self.__headers += f"Connection: {'keep-alive' if self.__server.keep_alive else 'close'}\n"
 
-        if self.type == "HTML":
-            self.headers += f"Content-Type: text/html; charset=UTF-8\n"
-        elif self.type == "JSON":
-            self.headers += f"Content-Type: application/json\n"
+        if type := self.__server.check_support_type(self.__type):
+            self.__headers += f"Content-Type: {type}"
+
+        self.__headers += "\r\n\r\n"
 
 
 class HTTPServer(object):
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s | %(levelname)s | %(message)s",
-        handlers=[
-            logging.FileHandler("server.log"),
-            logging.StreamHandler(),
-        ],
-    )
 
-    def __init__(self, host: str = "localhost", port: int = 9000, static: str = "./static", name: str = "Python3"):
+    def __init__(self, host: str = "localhost", port: int = 9000, static: str = "/static",
+                 static_supported_format: tuple = None, name: str = "Python3", allowed_methods: list = None,
+                 keep_alive: bool = True, debug: bool = True):
+
+        logging.basicConfig(
+            level=logging.DEBUG if debug is True else logging.INFO,
+            format="%(asctime)s | %(levelname)s | %(message)s",
+            handlers=[
+                logging.FileHandler("server.log"),
+                logging.StreamHandler(),
+            ],
+        )
+
+        if allowed_methods is None:
+            allowed_methods = ["GET"]
         self.name = name
-        self.static = static
         self.host = host
         self.port = port
+        self.__keep_alive = keep_alive
+        self.__static_supported_format = static_supported_format
+        self.__supported_types = dict(HTML="text/html; charset=UTF-8", JSON="application/json", JPG="image/jpeg",
+                                      PNG="image/png", ICO="image/x-icon", MP3="audio/mpeg", SVG="image/svg+xml",
+                                      WOFF="font/woff", WOFF2="font/woff2", MP4="video/mpeg", TTF="font/ttf",
+                                      JPEG="image/jpeg", GIF="image/gif", ZIP="application/zip",
+                                      RAR="application/x-rar-compressed", RTF="application/rtf", SH="application/x-sh",
+                                      DOC="application/msword", XLS="application/vnd.ms-excel",
+                                      PPT="application/vnd.ms-powerpoint", MID="audio/midi", MIDI="audio/midi",
+                                      WAV="audio/x-wav", AVI="video/x-msvideo", BIN="application/octet-stream",
+                                      CSS="text/css", CSV="text/csv", EPUB="application/epub+zip", ICS="text/calendar",
+                                      JS="application/javascript", MPKG="application/vnd.apple.installer+xml",
+                                      PDF="application/pdf", TAR="application/x-tar", XML="application/xml")
+        self.__allowed_methods = allowed_methods
+        self.__static = static
+        self.__path = "/".join(os.path.abspath(__file__).split("/")[:-1])
         self.__urls = dict()
         self.__loop = asyncio.get_event_loop()
-        self.__tasks = self.__loop.create_server(lambda: HTTPHandler(self), self.host, self.port)
+        self.__tasks = self.__loop.create_server(lambda: HTTPHandler(self, self.__loop), self.host, self.port)
         self.__server = self.__loop.run_until_complete(self.__tasks)
+
+    @property
+    def path(self) -> str:
+        """
+        Возвращает путь до директории сервера
+
+        :return:
+        """
+
+        return self.__path
+
+    @property
+    def static(self) -> str:
+        """
+        Возвращает путь статической директории директории сервера
+
+        :return: /static
+        """
+
+        return self.__static
+
+    @property
+    def static_path(self) -> str:
+        """
+        Возвращает путь до статической директории
+
+        :return: /user/test/static
+        """
+
+        return self.__path + self.__static
+
+    @property
+    def allowed_methods(self) -> list:
+        """
+        Возвращает разрешенные методы сервера
+
+        :return:
+        """
+
+        return self.__allowed_methods
+
+    @property
+    def static_supported_format(self) -> tuple:
+        """
+        Возвращает поддерживаемые форматы сервером в папке static
+
+        :return:
+        """
+
+        return self.__static_supported_format
+
+    @property
+    def keep_alive(self) -> bool:
+        """
+        Возвращает сведение о использовании keep_alive
+
+        :return:
+        """
+
+        return self.__keep_alive
 
     def run(self) -> None:
         """
@@ -222,9 +381,9 @@ class HTTPServer(object):
         :return:
         """
 
-        def decorator(f):
-            self.__urls[rule] = dict(**options, **dict(route=f))
-            return f
+        def decorator(function):
+            self.__urls[rule] = dict(**options, **dict(route=function))
+            return function
 
         return decorator
 
@@ -239,6 +398,16 @@ class HTTPServer(object):
         if url in self.__urls:
             return self.__urls[url]
         return None
+
+    def check_support_type(self, type: str) -> str or None:
+        """
+        Проверяет поддержку формата у сервера
+
+        :param type:
+        :return:
+        """
+
+        return self.__supported_types.get(type)
 
     @staticmethod
     def http_date(dt: datetime) -> str:
