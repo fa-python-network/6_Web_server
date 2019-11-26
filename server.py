b0f862f9eb2b71a4a32da9b305e93e892060fc25
StepIgor <45178763+StepIgor@users.noreply.github.com>
2019-11-26 15:01:10 +0300

    Универсальный заголовок + поддержка расширений: html, txt, изображения.

--- server.py
+++ server.py
@@ -14,9 +14,7 @@ sock.listen(5)
 workdir = os.getcwd()
 workdir = os.path.join(workdir,'www')
 
-header = 'HTTP/1.1 200 OK\nServer: StepIgorWebServer v1.0.0\nContent-type: text/html; charset:utf-8\nConnection: close\n\n'
-error404header = 'HTTP/1.1 404 Not Found\nServer: StepIgorWebServer v1.0.0'
-error403header = 'HTTP/1.1 403 Forbidden\nServer: StepIgorWebServer v1.0.0'
+header = ['HTTP/1.1',' ','2(code and word)','\nServer: StepIgorWebServer v1.0.0\nContent-type: ','4(content type)','; charset:utf-8\nConnection: close\n\n']
 
 while True:
 
@@ -27,7 +25,8 @@ while True:
 
 	print(msg)
 	
-	askfile = msg.split('\n')[0].split(' ')[1]
+	if (msg):
+		askfile = msg.split('\n')[0].split(' ')[1]
 	ansfile = ''
 	
 	#work with FS
@@ -35,21 +34,56 @@ while True:
 	if (askfile == '/'):
 		with open(os.path.join(workdir,'index.html'),'r', encoding='UTF-8') as f:
 			ansfile = f.read()
-		answer = header + ansfile
+		header[2] = '200 OK'
+		header[4] = 'text/html'
+		answer = ''.join(header) + ansfile
 		conn.send(answer.encode())
 	else:
 		askfile = askfile[1:len(askfile)]
+		
+		try:
+			if (askfile.split('.')[1]):
+				ext = askfile.split('.')[1]
+		except:
+			ext = 'none'
+			
 		if os.path.exists(os.path.join(workdir,askfile)):
 			if os.path.isfile(os.path.join(workdir,askfile)):
-				with open(os.path.join(workdir,os.path.join(workdir,askfile)),'r', encoding='UTF-8') as f:
-					ansfile = f.read()
-				answer = header + ansfile
-				conn.send(answer.encode())
+				if ext == 'html':
+					with open(os.path.join(workdir,os.path.join(workdir,askfile)),'r', encoding='UTF-8') as f:
+						ansfile = f.read()
+					header[2] = '200 OK'
+					header[4] = 'text/html'
+					answer = ''.join(header) + ansfile
+					conn.send(answer.encode())
+				elif (ext in ['gif','png','ico','jpg','jpeg']):
+					with open(os.path.join(workdir,os.path.join(workdir,askfile)),'rb') as f:
+						ansfile = f.read()
+						header[2] = '200 OK'
+						header[4] = 'image/'+ext
+						answer = ''.join(header).encode() + ansfile
+						conn.send(answer)
+				elif (ext == 'txt'):
+					with open(os.path.join(workdir,os.path.join(workdir,askfile)),'r', encoding='UTF-8') as f:
+						ansfile = f.read()
+					header[2] = '200 OK'
+					header[4] = 'text/txt'
+					answer = ''.join(header) + ansfile
+					conn.send(answer.encode())
+				else:
+					header[2] = '200 OK'
+					header[4] = 'text/html'
+					answer = ''.join(header) + 'Сервер не умеет отдавать файлы такого формата :('
+					conn.send(answer.encode())
 			else:
-				answer = error403header
+				header[2] = '403 Forbidden'
+				header[4] = 'text/html'
+				answer = ''.join(header)
 				conn.send(answer.encode())
 		else:
-			answer = error404header
+			header[2] = '404 Not Found'
+			header[4] = 'text/html'
+			answer = ''.join(header)
 			conn.send(answer.encode())
 	
 
