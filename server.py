cd961247141850af501d8661981f40356c6c603c
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2019-11-26 15:16:21 +0300

    пот
    жоский

--- server.py
+++ server.py
@@ -1,24 +1,36 @@
 import socket
 import re
 
-
+head = ['HTTP/1.1 200 OK', 'Server: Pokekek 0.02', 'Content-type: text/html', 'Connection: close', '', '']
 PAT = re.compile(r'[A-Z]{3,6}\s[\S]*\sHTTP/[0-9.]*', flags=re.MULTILINE) 
-
+htmllist = ['index.html', 'favicon.ico']
 class Response:
     def __init__(self, html ):
-        with open(html[1:], 'r', encoding='UTF-8') as file:
-            self.html = file.read()
-    
+        self.head = head
+        if html[1:] in htmllist:
+            if 'ico' in html:
+                self.head[2] = 'Content-type: image/ico'
+                with open(html[1:], 'rb') as file:
+                    self.html = file.read()
+            else:
+                with open(html[1:], 'r', encoding='UTF-8') as file:
+                    self.html = file.read()
+        elif html == '/':
+            with open('index.html', 'r', encoding='UTF-8') as file:
+                self.html = file.read()
+        else:
+            with open('322.html', 'r', encoding='UTF-8') as file:
+                self.html = file.read()
+                self.head[0] = 'HTTP/1.1 404'
+        self.head[5] = self.html 
+        print(' '.join(self.head))
     def __str__(self):
-        return f"""HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-{self.html}"""
+        return ' '.join(self.head)
 
     def __call__(self):
         return str(self)
+
+    
 
 
 
@@ -47,7 +59,8 @@ while True:
     msg = data.decode()
 
     print(msg)
-    html = parse(msg)
+    if msg:
+        html = parse(msg)
 
     conn.send(Response(html)().encode())
 
