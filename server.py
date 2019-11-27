c5a64dd70b16747608366d5d3dd656f7cf4a436e
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-11-27 21:44:13 +0300

    Основное задание

--- server.py
+++ server.py
@@ -1,31 +1,65 @@
 import socket
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
+import os
+from threading import Thread
+import time
 
+TYPEDICT = {"html" : "text/html; charset=UTF-8",
+        "jpg" : "image/jpeg",
+        "css" : "text/css",
+        "ico" : "image/x-icon"}
+
+
+def getDate():
+    now = datetime.now()
+    stamp = mktime(now.timetuple())
+    return format_date_time(stamp)
+
+def checkType(name,nameType)-> str:
+    resp = ""
+    requestType = ""
+    try:
+        requestType = TYPEDICT[nameType]
+        resp += f"HTTP/1.1 200 OK\n"
+    except: 
+        requestType = nameType
+        resp += f"HTTP/1.1 403 Forbidden\n"
+    date = getDate()
+    size = os.path.getsize(name)
+    resp += f"""Date: {date}
+    Server: MyServer
+    Content-type: {requestType}
+    Content-Length: {size}
+    Connection: close
+
+     """ 
+    return resp
+
+def listenToClient(conn,addr):
+        data = conn.recv(8192)
+        msg = data.decode()
+        print(f"KEKEKEK - {msg}")
+        name = msg.split()[1][1:]
+        if name == "" : name = "1.html"
+        resp = checkType(name,name.split('.')[-1])
+        with open(name, "r") as f:
+	        resp += f.read()
+        conn.send(resp.encode())
+        print(getDate())
+        conn.close()
+
+port = int(input("Порт:"))
 sock = socket.socket()
-
 try:
-    sock.bind(('', 80))
-    print("Using port 80")
+    sock.bind(('', port))
 except OSError:
     sock.bind(('', 8080))
-    print("Using port 8080")
-
 sock.listen(5)
+while True:
+    print("Kik")
+    conn, addr = sock.accept()
+    print("Connected", addr)
+    Thread(target = listenToClient,args = (conn,addr)).start()
 
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
