c10ec3cf086a0b0a80ab4bd29f5cacf69069b90c
LiliyaRachkova <lili.rachkova@ya.ru>
2019-11-27 21:22:25 +0300

    new commit

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import datetime
 
 def read_file(name):
     content = ''
@@ -9,7 +10,7 @@ def read_file(name):
     for line in testFile:
         if line.find('<img')!= -1:
             z = line.split('"')
-            z[1] = os.path.join(os.getcwd(), z[1])
+            z[1] = os.path.join('http://localhost', z[1])
             line = ''
             for i in z:
                 line += i + '"'
@@ -18,7 +19,12 @@ def read_file(name):
 
         content += line
     testFile.close()
-    return content
+    return content.encode()
+def read_img(name):
+    f = open(name, 'rb')
+    c = f.read()
+    f.close()
+    return c
 
 sock = socket.socket()
 
@@ -40,19 +46,43 @@ while True:
     print(msg)
     try:
         x = msg.split('\n')[0].split(' ')[1]
-        print(x)
-        print(os.getcwd())
-        if os.path.exists(os.path.join(os.getcwd(), x[1:])):
+        p = os.path.exists(os.path.join(os.getcwd(), x[1:]))
+        type = 'text/html'
+        now = datetime.datetime.now()
+        if p and x.split('.')[-1] == 'html':
             x = read_file(x[1:])
+            type = 'text/html'
+            status_code  = '200 OK'
+        elif p and x.split('.')[-1] == 'jpg':
+            x = read_img(x[1:])
+            type = 'image/jpeg'
+        elif p and x.split('.')[-1] == 'png':
+            x = read_img(x[1:])
+            type = 'image/png'
+        elif p and x.split('.')[-1] == 'gif':
+            x = read_img(x[1:])
+            type = 'image/gif'
+        elif not p:
+            status_code = '404'
+            x = read_file('404.html')
+            type = 'text/html'
+        elif p:
+            status_code = '403'
+            x = read_file('403.html')
+            type = 'text/html'
+        else:
+            x = x.encode()
 
-        resp = f""""HTTP/1.1 200 OK
+        resp = f"""HTTP/1.1 {status_code}
 Server: SelfMadeServer v0.0.1
-Content-type: text/html
+Content-type: {type}
+Content-length: {len(x)}
+Data:{now}
 Connection: close
 Charset: utf-8
 
-{x}"""
+"""
     except IndexError:
         pass
-    conn.send(resp.encode())
+    conn.send(resp.encode()+x)
     conn.close()
