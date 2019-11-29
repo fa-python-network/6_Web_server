dc0ebacfdea153a1fd5a2f4332a0e4053e4416e1
nastyapl77 <55870686+nastyapl77@users.noreply.github.com>
2019-11-29 17:13:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,57 @@
 import socket
+from threading import Thread
+import os
+
+
+def main():
+    def process_client(conn, addr):
+        print(f'Connected to "{addr}"')
+
+        try:
+            msg = conn.recv(8192).decode()
+            print(f'Received "{msg}" from "{addr}"\n')
+
+            header = msg.split('\n')[0]
+            path = header.split(' ')[1].strip('/')
+
+            with open(os.path.join(work_dir, path if path else 'index.html')) as f:
+                parsed_path = path.split('.')
+                cont_type = 'text/' + parsed_path[-1].lower() if len(parsed_path) > 1 else 'html' + '; charset=utf-8'
+
+                resp = f"""HTTP/1.1 200 OK
+Server: MyBestServer
+Content-Type: {cont_type}
+Connection: close
+
+{f.read()}"""
+        except BaseException as err:
+            print(err)
+            conn.send("""HTTP/1.1 500 Internal Server Error
+Server: MyBestServer
+Content-Type: text/html; charset=utf-8
+Connection: close
+
+<h1>Внутренняя ошибка сервера</h1>""".encode())
+            conn.close()
+        else:
+            conn.send(resp.encode())
+            conn.close()
+
+    sock.listen(5)
+
+    while True:
+        Thread(target=process_client, args=sock.accept()).start()
+
+
+work_dir = os.path.join(*['work_dir', 'templates'])
 
 sock = socket.socket()
 
+host, port = 'localhost', 80
 try:
-    sock.bind(('', 80))
-    print("Using port 80")
+    sock.bind((host, port))
+    print(f"Based on {host}:{port}")
 except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
-
-sock.listen(5)
-
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
+    print(f"{host}:{port} unavailable")
+else:
+    main()
