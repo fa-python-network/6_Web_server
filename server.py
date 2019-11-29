6702b1d60fad8961c7989252d60915b83f387849
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2019-11-29 11:40:54 +0300

    ИКОнКААА
    ЕЕЕЕЕЕ ботч

--- server.py
+++ server.py
@@ -4,43 +4,37 @@ import re
 head = ['HTTP/1.1 200 OK', 'Server: Pokekek 0.02', 'Content-type: text/html', 'Connection: close', '', '']
 PAT = re.compile(r'[A-Z]{3,6}\s[\S]*\sHTTP/[0-9.]*', flags=re.MULTILINE) 
 htmllist = ['index.html', 'favicon.ico']
+
+
 class Response:
     def __init__(self, html ):
         self.head = head
         if html[1:] in htmllist:
-            if 'ico' in html:
+            if 'favicon.ico' in html:
                 self.head[2] = 'Content-type: image/ico'
-                with open(html[1:], 'rb') as file:
-                    self.html = file.read()
-            else:
-                with open(html[1:], 'r', encoding='UTF-8') as file:
-                    self.html = file.read()
+            with open(html[1:], 'rb') as file:
+                self.html = file.read()
         elif html == '/':
-            with open('index.html', 'r', encoding='UTF-8') as file:
+            with open('index.html', 'rb') as file:
                 self.html = file.read()
         else:
-            with open('322.html', 'r', encoding='UTF-8') as file:
+            with open('322.html', 'rb') as file:
                 self.html = file.read()
-                self.head[0] = 'HTTP/1.1 404'
-        self.head[5] = self.html 
-        print(' '.join(self.head))
-    def __str__(self):
-        return ' '.join(self.head)
-
-    def __call__(self):
-        return str(self)
+                self.head[0] = 'HTTP/1.1 404 Not Found'
 
     
+    def __str__(self):
+        return '\n'.join(self.head)
 
+    def __call__(self):
+        return str(self).encode('UTF-8') + self.html
 
 
 def parse(req):
     return PAT.findall(req)[0].split(' ')[1]
 
 
-
 sock = socket.socket()
-
 
 
 try:
@@ -61,7 +55,6 @@ while True:
     print(msg)
     if msg:
         html = parse(msg)
-
-    conn.send(Response(html)().encode())
+    conn.send(Response(html)())
 
     conn.close()
