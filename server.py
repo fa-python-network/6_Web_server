dca558b94eef00aa33df6ad4e172dc097427d260
nikitinroman <48582860+nikitinroman@users.noreply.github.com>
2019-11-29 11:42:28 +0300

    Никитин, доделал лабу
    осталось фавикон добавить.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+from threading import *
 
 logging.basicConfig(
             level=logging.INFO,
@@ -9,16 +10,18 @@ logging.basicConfig(
                 logging.StreamHandler(),
             ],
         )
-with open('settings.txt', 'r+') as f:
+with open('settings.txt') as f:
     for line in f:
         HOST = line.split(';')[0]
         PORT = int(line.split(';')[1])
         max_size = int(line.split(';')[2])
+with open('favicon.png', 'rb') as file:
+    x = file.open()
 
 sock = socket.socket()
 with open('temp/index.html') as file:
     htm1 = file.read()
-error404header = 'HTTP/1.1 404 Not Found'
+error404header = 'HTTP/1.1 404 Not Found' + x
 error403header = 'HTTP/1.1 403 Forbidden Error'
 
 try:
@@ -30,62 +33,65 @@ except OSError:
 
 sock.listen(5)
 
-
-def response():
-    try:
-        with open('temp/' + msg1[1:]) as file:
-            htm = file.read()
-        resp = """HTTP/1.1 200 OK
-            Server: SelfMadeServer v0.0.1
-            Content-type: text/html
-            Connection: close
-
-            """ + htm
-    except:
-        resp = """HTTP/1.1 404 Not Found
-            Server: SelfMadeServer v0.0.1
-            Content-type: text/html
-            Connection: close
-
-            """ + error404header
-        logging.error(f'{addr[1]}, {msg1[1:]} raised Error 404 ')
-    finally:
-        if msg1[1:] == "":
-            resp = """HTTP/1.1 200 OK
-                        Server: SelfMadeServer v0.0.1
-                        Content-type: text/html
-                        Connection: close
-
-                        """ + htm1
-    print(msg)
-    conn.send(resp.encode())
-
-
-while True:
-    conn, addr = sock.accept()
+def new_client(conn, addr):
     print("Connected", addr)
 
     data = conn.recv(max_size)
     msg = data.decode()
-    print("->>>>", msg.split(" "))
     msg1 = msg.split(" ")[1]
 
-    print('[[[[[[[[[[[[[[[', msg1, ']]]]]]]]]]]]]]]]]]]]]]]')
     try:
         msg = msg1.split('.')
     except:
         pass
-    print('--------------------------------------', msg)
-    if len(msg) == 1 or msg[1] == 'html' or msg[1] == 'js' or msg[1] == 'css':
-        response()
-    else:
-        resp = """HTTP/1.1 403 Forbidden Error
-                    Server: SelfMadeServer v0.0.1
-                    Content-type: text/html
-                    Connection: close
 
-                    """ + error403header
+    if len(msg) == 1 or msg[1] == 'html' or msg[1] == 'js' or msg[1] == 'css':
+        response(msg,msg1)
+    else:
+        resp = """HTTP/1.1 403 Forbidden
+Server: SelfMadeServer v0.0.1
+Content-type: text/html
+Connection: close
+
+""" + error403header
         logging.error(f'{addr[1]}, {msg1[1:]} raised Error 403 ')
         conn.send(resp.encode())
+
+def response(msg,msg1):
+    try:
+        with open('temp/' + msg1[1:]) as file:
+            htm = file.read()
+        resp = """HTTP/1.1 200 OK
+Server: SelfMadeServer v0.0.1
+Content-type: text/html
+Connection: close
+
+"""+htm
+    except:
+        resp = """HTTP/1.1 404 Not Found
+Server: SelfMadeServer v0.0.1
+Content-type: text/html
+Connection: close
+
+"""+error404header
+
+        logging.error(f'{addr[1]}, {msg1[1:]} raised Error 404 ')
+    finally:
+        if msg1[1:] == "":
+            resp = """HTTP/1.1 200 OK
+Server: SelfMadeServer v0.0.1
+Content-type: text/html
+Connection: close
+
+"""+htm1
+
+    print(msg)
+    conn.send(resp.encode())
+
+
+
+while True:
+	conn, addr = sock.accept()
+	Thread(target=new_client, args=(conn, addr)).start()
 
 conn.close()
