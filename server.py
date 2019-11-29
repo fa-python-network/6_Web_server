3322d3413122693e276d07f172caa044ab3f7f55
silverdoggos <55032919+silverdoggos@users.noreply.github.com>
2019-11-29 14:07:30 +0300

    with binary types

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import os
 import threading
 import logging
 
+
 logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 sock = socket.socket()
@@ -25,8 +26,6 @@ def newclient (conn, addr, max, dir):
     while True:
         print("Connected", addr)
 
-        mess = ''
-
         data = conn.recv(max)
         msg = data.decode()
         msg = msg.split(' ')[1]
@@ -38,7 +37,10 @@ def newclient (conn, addr, max, dir):
         if msg[-1] == '?':
             msg = msg[:-1]
 
-        rash = msg.split('.')[1]
+        try:
+            rash = msg.split('.')[1]
+        except:
+            rash = ''
 
         if rash not in ('html', 'css', 'js', 'min', 'png'):
             logging.info(f' {msg} - {addr[0]} - 403 forbidden')
@@ -49,37 +51,41 @@ def newclient (conn, addr, max, dir):
 
         if rash == 'css' or rash == 'min':
             Content = "text/css"
-        elif rash == 'html':
+        elif rash == 'html' or rash == 'htm':
             Content = "text/html"
         elif rash == 'png':
             Content = "image/png"
+        elif rash == 'jpeg':
+            Content = "image/jpeg"
         else:
             Content = ""
 
         try:
-            with open(os.path.join(dir, msg), 'r+') as f:
-                logging.info(f'{msg} - {addr[0]} - 200 OK')
-                for line in f:
-                    mess += line
+            logging.info(f'{msg} - {addr[0]} - 200 OK')
+            with open(dir + msg, 'rb') as f:
+                mess = f.read()
+                leng = os.path.getsize(msg)
+
+
         except FileNotFoundError:
              e = 404
              logging.info(f'{msg} - {addr[0]} - 404')
-             with open('./404.htm', 'r+') as f:
-                 for line in f:
-                     mess += line
+             with open('404.htm', 'rb') as f:
+                 mess = f.read()
+                 leng = os.path.getsize('404.htm')
+
 
         resp = f"""HTTP/1.1 {e} OK
         Date: {time.ctime()}
         Server: SelfMadeServer v0.0.1
         Content-type: {Content}
-        Content-Length: {len(mess.encode())}
+        Content-Length: {leng}
         Connection: close
 
+"""
 
-{mess}"""
 
-        conn.send(resp.encode())
-
+        conn.send(resp.encode() + mess)
         conn.close()
         return
         
