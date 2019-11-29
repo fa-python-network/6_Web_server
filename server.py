484326e6e8a00cf21dc76e6c6d413f0726f1e70d
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-11-29 06:47:35 +0300

    Основное задание + допы

--- server.py
+++ server.py
@@ -1,65 +1,61 @@
-import socket
-from wsgiref.handlers import format_date_time
-from datetime import datetime
-from time import mktime
 import os
+import socket
+from datetime import datetime
 from threading import Thread
-import time
 
-TYPEDICT = {"html" : "text/html; charset=UTF-8",
-        "jpg" : "image/jpeg",
-        "css" : "text/css",
-        "ico" : "image/x-icon"}
 
+typeDict = dict(html="text/html; charset=UTF-8", jpg="image/jpeg", css="text/css", ico="image/x-icon")
 
 def getDate():
-    now = datetime.now()
-    stamp = mktime(now.timetuple())
-    return format_date_time(stamp)
+	return datetime.now().strftime("%a, %d %b 20%y %X GMT")
 
-def checkType(name,nameType)-> str:
-    resp = ""
-    requestType = ""
-    try:
-        requestType = TYPEDICT[nameType]
-        resp += f"HTTP/1.1 200 OK\n"
-    except: 
-        requestType = nameType
-        resp += f"HTTP/1.1 403 Forbidden\n"
-    date = getDate()
-    size = os.path.getsize(name)
-    resp += f"""Date: {date}
-    Server: MyServer
-    Content-type: {requestType}
-    Content-Length: {size}
-    Connection: close
+def checkType(name, nameType) -> str:
+	try:
+		requestType = typeDict[nameType]
+		resp = f"HTTP/1.1 200 OK\n"
+	except:
+		requestType = nameType
+		date = getDate()
+		resp = f"""HTTP/1.1 403 Forbidden
+		
+Date: {date}
+Server: MyServer
 
-     """ 
-    return resp
+"""
+		return resp.encode()
+	date = getDate()
+	size = os.path.getsize(name)
+	resp += f"""Date: {date}
+Server: MyServer
+Content-type: {requestType}
+Content-Length: {size}
 
-def listenToClient(conn,addr):
-        data = conn.recv(8192)
-        msg = data.decode()
-        print(f"KEKEKEK - {msg}")
-        name = msg.split()[1][1:]
-        if name == "" : name = "1.html"
-        resp = checkType(name,name.split('.')[-1])
-        with open(name, "r") as f:
-	        resp += f.read()
-        conn.send(resp.encode())
-        print(getDate())
-        conn.close()
+"""
+	return resp.encode()
 
+def listenToClient(conn, addr):
+	data = conn.recv(8192)
+	msg = data.decode()
+	name = msg.split()[1][1:]
+	if name == "": name = "1.html"
+	resp = checkType(name, name.split('.')[-1])
+	try:
+		with open(name, "rb") as f:
+			resp += f.read()
+	except:
+		resp += "403 Forbidden Format".encode()
+	conn.send(resp)
+	conn.close()
+	
 port = int(input("Порт:"))
 sock = socket.socket()
+
 try:
-    sock.bind(('', port))
+	sock.bind(('', port))
 except OSError:
-    sock.bind(('', 8080))
+	sock.bind(('', 8080))
 sock.listen(5)
 while True:
-    print("Kik")
-    conn, addr = sock.accept()
-    print("Connected", addr)
-    Thread(target = listenToClient,args = (conn,addr)).start()
-
+	conn, addr = sock.accept()
+	print("Connected", addr)
+	Thread(target=listenToClient, args=(conn, addr)).start()
