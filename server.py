1193a4f3f154be62d32a1d7e0b4e0e46ae488f46
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2019-11-30 19:49:02 +0300

    Логи, настройки
    НАДА ыщо с потоками разобраца

--- server.py
+++ server.py
@@ -1,60 +1,90 @@
 import socket
 import re
+import json
+import logging
+import os
+import threading
+import datetime
+from time import sleep
 
-head = ['HTTP/1.1 200 OK', 'Server: Pokekek 0.02', 'Content-type: text/html', 'Connection: close', '', '']
-PAT = re.compile(r'[A-Z]{3,6}\s[\S]*\sHTTP/[0-9.]*', flags=re.MULTILINE) 
+def openfi(filename, type='rb'): # Открытие файлов.
+    with open(filename, type) as file:
+                return file.read()
+
+
+with open('config.json') as file:
+    config = json.load(file)
+MAX = config['MAX'] # Максимум бит в одном запросе
+PORT = config['PORT'] # Порт
+DIR = config['DIR'] # Рабочая директория
+LIS = config['LIS'] # Кол-во прослушиваемых
+ADRESS = config['ADR']
+os.chdir(DIR)
+logging.basicConfig(filename="log_serv", level=logging.INFO) # Логирование
+
+head = ['HTTP/1.1 200 OK', 'Server: Pokekek 0.02', 'Content-type: text/html', 'Connection: close', '', ''] # Список редактируемый в процессе.
+PAT = re.compile(r'[A-Z]{3,6}\s[\S]*\sHTTP/[0-9.]*', flags=re.MULTILINE) #  Создание паттерна
 htmllist = ['index.html', 'favicon.ico']
 
 
-class Response:
+
+
+
+class Response: #  Класс овтета, формируется на основе запроса.
     def __init__(self, html ):
         self.head = head
-        if html[1:] in htmllist:
+        logging.info(f'Запрос: {html}')
+        if html[1:] in htmllist: #  Базовая операция доставания файла.
             if 'favicon.ico' in html:
                 self.head[2] = 'Content-type: image/ico'
-            with open(html[1:], 'rb') as file:
-                self.html = file.read()
-        elif html == '/':
-            with open('index.html', 'rb') as file:
-                self.html = file.read()
-        else:
-            with open('322.html', 'rb') as file:
-                self.html = file.read()
-                self.head[0] = 'HTTP/1.1 404 Not Found'
+            self.html = openfi(html[1:])
+        elif html == '/': #  Страница по умолчанию.
+            self.html = openfi('index.html')
+        else:  #  Ветка ошибки.
+            self.html = openfi('322.html')
+            self.head[0] = 'HTTP/1.1 404 Not Found'
+        logging.info(self.head[0])
+        logging.info(' ')
+        print(self.head)
+        
 
     
-    def __str__(self):
-        return '\n'.join(self.head)
-
-    def __call__(self):
-        return str(self).encode('UTF-8') + self.html
+    def resp(self):
+        self.head = '\n'.join(self.head)
+        return self.head.encode('UTF-8') + self.html
 
 
 def parse(req):
-    return PAT.findall(req)[0].split(' ')[1]
+    return PAT.findall(req)[0].split(' ')[1] #  Функция обработки запроса с помощью паттерна
+
+
+def answer(msg, conn):
+    html = parse(msg)
+    Ans = Response(html)
+    conn.send(Ans.resp())
+    conn.close()
 
 
 sock = socket.socket()
 
 
 try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
+    sock.bind((ADRESS, PORT))
     print("Using port 8080")
+except OSError:
+    sock.bind((ADRESS, 80))
+    print("Using port 80")
 
-sock.listen(5)
+sock.listen(LIS)
 while True:
     conn, addr = sock.accept()
-    print("Connected", addr)
-
-    data = conn.recv(8192)
+    now = datetime.datetime.now()
+    logging.info(now)
+    logging.info(f"Connected {addr}")
+    sleep(0.01)
+    data = conn.recv(MAX)
     msg = data.decode()
-
     print(msg)
     if msg:
-        html = parse(msg)
-    conn.send(Response(html)())
-
-    conn.close()
+        work = threading.Thread(target=answer, args=[msg, conn])
+        work.start()
