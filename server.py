5c0c882b310865f6eb9bd9ea8da202f9a9ba39d1
MacKenzie Malikov <vmolchanovag@mail.ru>
2019-12-03 02:18:24 +0300

    image_reader

--- server.py
+++ server.py
@@ -9,6 +9,13 @@ def file_reader(name):
     file.close()
     return content.encode()
 
+def image_reader(name):
+    file = open(name, 'rb')
+    content= file.read()
+    file.close()
+    return content
+    
+
 
 sock = socket.socket()
 
@@ -43,16 +50,22 @@ while True:
             conn.send(file_reader(path_to_file))
         elif path_to_file.split('.')[1] == 'img':
             type_of_file = 'image/jpeg'
-            conn.send(file_reader(path_to_file))
+            conn.send(image_reader(path_to_file))
+        elif path_to_file.split('.')[1] == 'jpg':
+            type_of_file = 'image/jpeg'
+            conn.send(image_reader(path_to_file))
+        elif path_to_file.split('.')[1] == 'png':
+            type_of_file = 'image/jpeg'
+            conn.send(image_reader(path_to_file))
 
     else:
-        resp = f"""HTTP/1.1 404 Not found
+        resp = """HTTP/1.1 404 Not found
     Server: SelfMadeServer v0.0.1
-    Content-type: {type}
-    Date: {date}
+    Content-type: {}
+    Date: {}
     Connection: close
-    Charset = UTF-8
-    """
+    Charset: UTF-8
+    """.format(type_of_file, date)
         conn.send(resp.encode())
 
 conn.close()
