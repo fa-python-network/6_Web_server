613285840c77993224f446291022d6d9cce554ab
krasotskaya <49969731+krasotskaya@users.noreply.github.com>
2019-12-16 13:46:29 +0300

    доп задания

--- server.py
+++ server.py
@@ -1,31 +1,101 @@
-import socket
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 15:59:25 2019
 
-sock = socket.socket()
+@author: Елизавета
+"""
 
+import socket, datetime, csv, threading
+
+class ClientThreading(threading.Thread):
+	def __init__(self,addr,conn,port):
+		threading.Thread.__init__(self)
+		self.conn = conn
+		self.addr = addr
+		self.port = port
+        
 try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
-
-sock.listen(5)
-
+    with open ("set.csv", "r", newline="") as sett:
+        reader = csv.reader(sett)
+        for row in reader:
+            if row[0] == "PORT":
+                PORT = int(row[1])
+            elif row[0] == "listen":
+                listen = int(row[1])
+            elif row[0] == "data_size":
+                data_size = int(row[1])
+            elif row[0] == "code":
+                code = row[1]
+except:
+    PORT = 8080
+    listen = 5
+    data_size = 8192
+    code = "404 NOT FOUND"
+    
+sock = socket.socket()
+sock.bind(('', PORT))
+print("Using port "+str(PORT))
+sock.listen(listen)
 conn, addr = sock.accept()
-print("Connected", addr)
+print("Connection", addr)
+newthread = ClientThreading(addr, conn, PORT)
+newthread.start()
+ 
 
-data = conn.recv(8192)
+data = conn.recv(data_size)
 msg = data.decode()
-
 print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
+try:
+    file_name = msg[msg.index("GET")+4:msg.index("HTTP")-1]
+    print(file_name[file_name.index(".")+1:])
+    if file_name[file_name.index(".")+1:] != "html" and file_name[file_name.index(".")+1:] != "css" and file_name[file_name.index(".")+1:] != "js" and file_name[file_name.index(".")+1:] != "png" and file_name[file_name.index(".")+1:] != "ico":
+        flag = False
+    else:
+        flag = True
+except:
+    print("че-то произошло")
+    file_name = "/index.html" 
+    flag = True 
+if flag:
+    try:
+        to_open = "C:/Users/Елизавета/Desktop"+file_name
+        f = open(to_open, "r")
+        msg_new = f.read()
+    except:
+        code = "404 NOT FOUND"
+else:
+    code = "403 FORBIDDEN"
+    
+date = datetime.datetime.today()
+print(date)
+if "404" in code:
+    msg = "Hello!"
+    resp = """HTTP/1.1 """+code+"""
+Date: """+str(date)+"""
 Content-type: text/html
-Connection: close
+Server: SelfMadeServer v0.0.1
+Content-length: 0
+Connection: Close
 
-Hello, webworld!"""
+404 FILE NOT FOUND"""
+    
+elif "403" in code:
+    resp = """HTTP/1.1 """+code+"""
+Date: """+str(date)+"""
+Content-type: text/html
+Server: SelfMadeServer v0.0.1
+Content-length:"""+str(len("403 FORBIDDEN"))+"""
+Connection: Close 
 
+403 FORBIDDEN"""
+else:
+    resp = """HTTP/1.1 """+code+"""
+Date: """+str(date)+"""
+Content-type: text/html
+Server: SelfMadeServer v0.0.1
+Connection: keep-alive
+
+"""+str(msg_new)
 conn.send(resp.encode())
-
+print(resp) 
 conn.close()
