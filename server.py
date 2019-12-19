f8a7f7a78d019992a30b996f3d7891d3766dcf51
Dafhog <dafhog35@gmail.com>
2019-12-19 21:24:02 +0300

    Егоров Пи18-1 исправил ошибки

--- server.py
+++ server.py
@@ -1,107 +1,87 @@
 import socket
-from settings import Settings
-import threading
-import datetime
 import os
+import datetime
+import logging
+from threading import Thread
 
-sock = socket.socket()
+from settings import Settings 
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
-def request(conn, addr, data, directory):
+
+def handler(msg):
+    _, url = msg.split(" ")[:2]  
+    file_type = url.split(".")[-1]  
+    date = datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S GTM')  
+    if os.path.isfile(Settings.directory + url):  
+        if file_type in Settings.types:  
+            with open(Settings.directory + url, "rb") as sf:
+                data = sf.read()
+                return f"HTTP/1.1 200\n" \
+                       f"Server: {Settings.name}\n" \
+                       f"Date: {date}\n" \
+                       f"Content-length: {len(data)}\n" \
+                       f"Content-type: {Settings.types[file_type]}\n" \
+                       f"Connection: keep-alive\n" \
+                       f"\r\n\r\n".encode() + data  
+        else:  
+            return f"HTTP/1.1 403\n" \
+                   f"Server: {Settings.name}\n" \
+                   f"Date: {date}\n" \
+                   f"Content-type: text/html; charset=UTF-8\n" \
+                   f"Connection: close\n" \
+                   f"\r\n\r\n".encode()
+    else:  
+        return f"HTTP/1.1 404\n" \
+               f"Server: {Settings.name}\n" \
+               f"Date: {date}\n" \
+               f"Content-type: text/html; charset=UTF-8\n" \
+               f"Connection: close\n" \
+               f"\r\n\r\n".encode()
+
+
+def connection(conn, addr):
+    while True:  
+
+        data = conn.recv(Settings.request_size) 
+
+        if data == b"":  
+            conn.close()
+            break
+
+        msg = data.decode()  
+
+        logging.info(str(addr[0]) + ":" + str(addr[1]) + " новый запрос")  
+
+        resp = handler(msg)  
+
+        conn.send(resp)  
+
+
+if __name__ == '__main__':  
+
+    logging.basicConfig(  
+        level=logging.DEBUG,
+        
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        handlers=[
+            logging.FileHandler("logs.log"),
+            logging.StreamHandler(),
+        ],
+    )
+
+    sock = socket.socket()  
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  
+
     
-    msg = data.decode()
-    print(msg)
+    sock.bind(('', Settings.port))  
+    logging.info("Запуск сервера на порте: " + str(Settings.port))
 
-sock.listen(5)
+    sock.listen(5)  
 
-name = msg.split()[1][1:]
-
-conn, addr = sock.accept()
-print("Connected", addr)
-if name == "":
-        name = "index.html"
-name = directory + "\\" + name
-
-
-now = datetime.datetime.now()
-date = now.strftime("%a, %d %b %Y %H:%M:%S GTM")
-
-    
-with open("log.txt", "a") as log:
-        print(f"Date: {date}\nAddr: {addr}\nFile: {name}", file=log)
-
-    
-try:
-        size = os.path.getsize(name)  
-
-  
-except FileNotFoundError:
-        resp = f"""HTTP/1.1 404 Not Found
-        Server:SelfMadeServer v0.0.1
-        Date: {date}
-        Connection: keep-alive
-        """
-        with open("log.txt", "a") as log:
-            print("Error: 404", file=log)
-
-data = conn.recv(8192)
-msg = data.decode()
-resp = resp.encode()
-
-decr = name.split(".")[-1]  
-if decr not in settings.types:
-            resp = f"""HTTP/1.1 403 Forbidden
-            Server:SelfMadeServer v0.0.1
-            Date: {date}
-            Connection: keep-alive
-            """
-            with open("log.txt", "a") as log:
-                print("Error: 403", file=log)
-else:
-            try:
-                with open(name, "r", encoding="utf-8") as file:
-                    resp = f"""HTTP/1.1 200 OK
-                    Server: SelfMadeServer v0.0.1
-                    Date: {date}
-                    Content-Type: text/{decr};charset=utf-8
-                    Content-Length: {size}
-                    Connection: keep-alive
-print(msg)
-                    """
-                    resp += file.read()
-                resp = resp.encode() 
-def connection(conn, addr, directory):
-    data = conn.recv(settings.request_size)
-    if not data:
-        return
-    request(conn, addr, data, directory)
-    conn.close()
-
-conn.close() 
-
-settings = Settings() 
-sock = socket.socket()
-
-
-try:
-    sock.bind(('', settings.port))
-    print(f"Using port {settings.port}")
-except OSError:
-    sock.bind(('', settings.port2))
-    print(f"Using port {settings.port2}")
-sock.listen(5)
-
-directory = settings.directory
-
-
-conn, addr = sock.accept()
-while True:
-    print("Connected", addr, "\n")
-    tr = threading.Thread(target=connection, args=(conn, addr, directory))
-    tr.start()
-    conn, addr = sock.accept() 
+    while True:  
+        conn, addr = sock.accept()  
+        
+        logging.info("Новое подключение: " + str(addr[0]) + ":" + str(addr[1]))  
+        flow = Thread(target=connection, args=(conn, addr))  
+        
+        flow.start()  
+       
--- settings.py
+++ settings.py
@@ -2,9 +2,15 @@ import os
 
 
 class Settings:
-    def __init__(self):
-        self.port = 80  
-        self.port2 = 8080  
-        self.directory = os.getcwd()  
-        self.request_size = 8192  
-        self.types = ['html', 'css', 'js', 'jpg', 'ico', 'gif', 'jpeg', 'png']  
+    port = 9001
+    directory = os.getcwd()
+    request_size = 8192
+    types = {'html': 'text/html; charset=UTF-8',
+             'css': 'text/css',
+             'js': 'application/javascript',
+             'jpg': 'image/jpeg',
+             'ico': 'image/x-icon',
+             'gif': 'image/gif',
+             'jpeg': 'image/jpeg',
+             'png': 'image/png'}
+    name = "SelfMadeServer v0.0.666"
