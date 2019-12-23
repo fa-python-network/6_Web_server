470f4ade9e90be35b326ca8444c623779c15aa93
Alex <siuziumov@mail.ru>
2019-12-24 00:12:48 +0300

    work 6

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 import socket
+import os
 
 sock = socket.socket()
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+port = 80
+while port != 65536:
+    try:
+        sock.bind(('', port))
+        break
+    except:
+        port += 1
 
+print('Port: ', port)
 sock.listen(5)
 
 conn, addr = sock.accept()
@@ -17,15 +20,26 @@ print("Connected", addr)
 data = conn.recv(8192)
 msg = data.decode()
 
-print(msg)
+msg1 = msg.split(' ')[1]
+print(msg1[1:])
+print(os.path.abspath('index.html'))
 
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
+if msg1 == '/':
+    file = open('index.html', 'r')
+    content = file.read()
+    conn.send(content.encode())
+    file.close()
+elif os.path.exists(os.path.abspath(str(msg1[1:]))) == True:
+    file = open(str(msg1[1:]), 'r')
+    content = file.read()
+    conn.send(content.encode())
+    file.close()
+else:
+    resp1 = """HTTP/1.1 404 NOT FOUND
+    Server: SelfMadeServer v0.0.1
+    Content-type: text/html
+    Connection: close
+    """
+    conn.send(resp1.encode())
 
 conn.close()
